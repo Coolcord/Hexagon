0f738bd3ae8948e0efc38563ea9ee5f9b6c2be19
Coolcord <coolcord24@tutanota.com>
2018-06-09 16:28:56 -0700

    Parser is now written

--- Hexagon/Parser.cpp
+++ Hexagon/Parser.cpp
@@ -1,5 +1,6 @@
 #include "Parser.h"
 #include "Patch_Strings.h"
+#include <assert.h>
 
 Parser::Parser(const QByteArray &patchBytes) {
     this->stream = new QTextStream(patchBytes, QIODevice::ReadOnly);
@@ -22,7 +23,7 @@ bool Parser::Get_Checksum(QString &checksum) {
     return true;
 }
 
-bool Parser::Get_Next_Offset_And_Value(qint64 offset, QByteArray &value) {
+bool Parser::Get_Next_Offset_And_Value(qint64 &offset, QByteArray &value) {
     //Get the Offset
     QString line = this->Get_Next_Line_After_Comments();
     if (!line.startsWith(Patch_Strings::STRING_OFFSET)) return false;
@@ -35,26 +36,73 @@ bool Parser::Get_Next_Offset_And_Value(qint64 offset, QByteArray &value) {
     if (!valid) return false;
 
     //Get the Value
-    line = this->Get_Next_Line_After_Comments();
-    if (!line.startsWith(Patch_Strings::STRING_VALUE)) return false;
-    values = line.split(' ');
-    if (values.size() != 2) return false;
-    QString valueString = values.at(1);
-    valueString = this->Trim_Hex_Identifier(valueString);
-    //TODO: Write this...
+    if (!this->Parse_Value(value)) return false;
+    return true;
+}
 
+bool Parser::Convert_QString_To_QByteArray(const QString &string, QByteArray &output) {
+    if (string.size()%2 == 1) return false;
+    output = QByteArray(string.size()/2, 0x00);
+    for (int i = 0, j = 0; i < string.size(); i += 2, ++j) {
+        assert(j < output.size());
+        QString numberString = QString(string.at(i))+QString(string.at(i+1));
+        bool valid = false;
+        int number = numberString.toInt(&valid, 0x10);
+        if (!valid) return false;
+        assert(numberString >= 0x00 && numberString <= 0xFF);
+        output.data()[j] = static_cast<char>(number);
+    }
+    return true;
 }
 
 QString Parser::Get_Next_Line_After_Comments() {
     QString line = QString();
-    do {
+    while (!this->stream->atEnd()) {
         ++this->currentLineNum;
         line = this->stream->readLine().trimmed();
         if (line.isEmpty()) continue;
         if (line.startsWith(Patch_Strings::STRING_COMMENT)) continue;
         else return line;
-    } while (!this->stream->atEnd());
+    }
     return QString(); //nothing left to read
+}
+
+bool Parser::Is_Line_Hex_String(const QString &line) {
+    for (int i = 0; i < line.size(); ++i) {
+        switch (line.at(i).toLower().toLatin1()) {
+        default: return false;
+        case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7':
+        case '8': case '9': case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': continue;
+        }
+    }
+    return true;
+}
+
+bool Parser::Parse_Value(QByteArray &value) {
+    //Parse the First Line
+    QString line = this->Get_Next_Line_After_Comments();
+    if (!line.startsWith(Patch_Strings::STRING_VALUE)) return false;
+    QStringList values = line.split(' ');
+    if (values.size() != 2) return false;
+    QString valueString = values.at(1);
+    valueString = this->Trim_Hex_Identifier(valueString);
+    if (!this->Is_Line_Hex_String(valueString)) return false;
+
+    //Parse Any Additional Lines
+    while (!this->stream->atEnd()) {
+        qint64 posBefore = this->stream->pos();
+        line = this->stream->readLine().trimmed();
+        if (line.startsWith(Patch_Strings::STRING_OFFSET)) {
+            assert(this->stream->seek(posBefore));
+            break;
+        } else {
+            ++this->currentLineNum;
+            line = this->Trim_Hex_Identifier(line);
+            if (!this->Is_Line_Hex_String(line)) return false;
+            valueString += line;
+        }
+    }
+    return this->Convert_QString_To_QByteArray(valueString, value);
 }
 
 QString Parser::Trim_Hex_Identifier(QString &hexString) {
--- Hexagon/Parser.h
+++ Hexagon/Parser.h
@@ -10,10 +10,13 @@ public:
     ~Parser();
     int Get_Current_Line_Num();
     bool Get_Checksum(QString &checksum);
-    bool Get_Next_Offset_And_Value(qint64 offset, QByteArray &value);
+    bool Get_Next_Offset_And_Value(qint64 &offset, QByteArray &value);
 
 private:
+    bool Convert_QString_To_QByteArray(const QString &string, QByteArray &output);
     QString Get_Next_Line_After_Comments();
+    bool Is_Line_Hex_String(const QString &line);
+    bool Parse_Value(QByteArray &value);
     QString Trim_Hex_Identifier(QString &hexString);
 
     QTextStream *stream;
