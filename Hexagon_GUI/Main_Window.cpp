b44aa9e901857b1e6623e88b744fe71e4cf62295
Coolcord <coolcord24@tutanota.com>
2018-06-12 02:09:25 -0700

    Output file locations are now built properly

--- Hexagon_GUI/Main_Window.cpp
+++ Hexagon_GUI/Main_Window.cpp
@@ -71,7 +71,7 @@ void Main_Window::on_btnApplyPatch_clicked() {
     }
 
     //Choose Where to Save the New File
-    QString outputFileLocation = patchFileInfo.path()+this->stringManipulator->Get_File_Name_Without_Extension(patchFileInfo.fileName())+this->stringManipulator->Get_Extension(originalFileInfo.fileName());
+    QString outputFileLocation = patchFileInfo.path()+"/"+this->stringManipulator->Get_File_Name_Without_Extension(patchFileInfo.fileName())+"."+this->stringManipulator->Get_Extension(originalFileInfo.fileName());
     QFileInfo outputFileInfo(outputFileLocation);
     if (this->ui->cbAlwaysAskForSaveLocation->isChecked() || outputFileInfo.exists()) {
         outputFileLocation = this->fileDialogManager->Get_Save_File_Location(File_Types::ANY_FILE, this->stringManipulator->Get_Extension(originalFileInfo.fileName()));
