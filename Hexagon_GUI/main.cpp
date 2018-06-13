92d9c7a1f047ae979bab10b7cd4b09b9eed65d42
Coolcord <coolcord24@tutanota.com>
2018-06-12 20:54:24 -0700

    Removed some unnecessary code

--- Hexagon_GUI/main.cpp
+++ Hexagon_GUI/main.cpp
@@ -6,17 +6,11 @@
 #include <QPluginLoader>
 #include <QMessageBox>
 
-void Show_Plugin_Error_Message() {
-    QMessageBox::critical(NULL, Common_Strings::STRING_HEXAGON, Common_Strings::STRING_HEXAGON +
-                             " is unable to load the backend plugin! The plugin may not be valid.",
-                          Common_Strings::STRING_OK);
-}
-
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     //Load the Hexagon Plugin
-    QString unableToLoadPluginMessage = Common_Strings::STRING_HEXAGON+" is unable to load the backend plugin! The plugin may not be valid.";
+    QString unableToLoadPluginMessage = Common_Strings::STRING_HEXAGON+" is unable to load the backend plugin! Make sure the plugin is in the plugins folder!";
     QPluginLoader *pluginLoader = new QPluginLoader(Common_Strings::STRING_PLUGIN_LOCATION);
     QObject *validPlugin = pluginLoader->instance();
     Error_Messages errorMessages;
