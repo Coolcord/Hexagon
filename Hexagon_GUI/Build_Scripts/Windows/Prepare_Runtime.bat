073a2eca4d4e32e9087e102b25d8cae22fe828b5
Coolcord <coolcord24@tutanota.com>
2024-10-12 21:38:18 -0700

    Updated Windows Prepare_Runtime.bat to Qt version 6.7.3

--- Hexagon_GUI/Build_Scripts/Windows/Prepare_Runtime.bat
+++ Hexagon_GUI/Build_Scripts/Windows/Prepare_Runtime.bat
@@ -1,16 +1,16 @@
 @ECHO OFF
 COLOR 0A
 
-SET QT=Qt_6_5_2_MinGW_64_bit
+SET QT=Qt_6_7_3_MinGW_64_bit
 SET MODE=%1
 IF %1.==. SET MODE=Debug
 SET SOURCE_LOCATION=D:\Documents\Source_Code
 
 REM Prepare Folders
-MKDIR "%SOURCE_LOCATION%\Hexagon\build-Hexagon_GUI-Desktop_%QT%-%MODE%\Plugins" >NUL 2>&1
+MKDIR "%SOURCE_LOCATION%\Hexagon\Hexagon_GUI\build\Desktop_%QT%-%MODE%\Plugins" >NUL 2>&1
 
 REM Delete Old Plugins
-ERASE "%SOURCE_LOCATION%\Hexagon\build-Hexagon_GUI-Desktop_%QT%-%MODE%\Plugins\Hexagon.dll" >NUL 2>&1
+ERASE "%SOURCE_LOCATION%\Hexagon\Hexagon_GUI\build\Desktop_%QT%-%MODE%\Plugins\Hexagon.dll" >NUL 2>&1
 
 REM Install New Plugins
-COPY "%SOURCE_LOCATION%\Hexagon\build-Hexagon-Desktop_%QT%-%MODE%\libHexagon.dll" "%SOURCE_LOCATION%\Hexagon\build-Hexagon_GUI-Desktop_%QT%-%MODE%\Plugins\Hexagon.dll"
+COPY "%SOURCE_LOCATION%\Hexagon\Hexagon\build\Desktop_%QT%-%MODE%\libHexagon.dll" "%SOURCE_LOCATION%\Hexagon\Hexagon_GUI\build\Desktop_%QT%-%MODE%\Plugins\Hexagon.dll"
